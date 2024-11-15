c33bc25e883154e240028995a790c322864db26c
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2024-11-15 09:57:23 +0300

    On completing two functional solutions for the Knight Travails problem.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-// import { format } from "date-fns";
-// import _ from 'lodash';
-console.log("Template works.")
+import { knightMoves } from "./app";
+import { knightMovesShortestPath } from "./appTwo";
+
+const start = [0, 0];  // Starting position
+const target = [7, 7];  // Target position
+
+const testPathOne = knightMoves(start, target);
+console.log(testPathOne); // [[0, 0], [1, 2], [2, 4], [3, 6], [4, 4], [5, 6], [7, 7]]
+
+const testPathTwo = knightMovesShortestPath(start, target);
+console.log(testPathTwo); // [[0, 0], [1, 2], [2, 4], [3, 6], [4, 4], [5, 6], [7, 7]]
